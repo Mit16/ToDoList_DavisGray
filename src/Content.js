7305b581b387b2489b67e0a49b94c1c6a4426041
alpha <amitk.vishwa1633@gmail.com>
2024-10-29 16:12:27 +0530

    upto this all is good functioning properly, remaining to add some css to make it look better, after this I am going to use the json server to load the file from the server insted of localstorage

--- src/Content.js
+++ src/Content.js
@@ -1,33 +1,8 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-const Content = () => {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One half bag of cocoa Covered almonds unsalted",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3",
-    },
-  ]);
+import ItemList from "./ItemList";
 
-  const handleCheck = (id) => {
-    // console.log(`key: ${id}`)
-    const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item
-    );
-    setItems(listItems);
-    // localStorage.setItem('shoppinglist'.JSON.stringify(listItems));
-  };
-
+const Content = ({ items, handleCheck, handleDelete }) => {
   // const [name,setName] = useState('Alpha');
   // const [count,setCount]= useState(0);
 
@@ -52,11 +27,6 @@ const Content = () => {
   //   console.log(e.target.innerText);
   //  }
 
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-  };
-
   return (
     <main>
       {/* <p onDoubleClick={handleClick}>Hello {name}!</p>
@@ -65,29 +35,15 @@ const Content = () => {
       <button onClick={()=>{handleClick2('Alpha')}}>Click It name</button>
       <button onClick={(e)=>{handleClick3(e)}}>Click It Event</button> */}
 
-        {items.length ? (
-
-      <ul>
-        {items.map((item) => (
-          <li className="item" key={item.id}>
-            <input
-              type="checkbox"
-              onChange={() => handleCheck(item.id)}
-              checked={item.checked}
-            />
-            <label
-              style={item.checked ? { textDecoration: "line-through" } : null}
-              onDoubleClick={() => handleCheck(item.id)}
-            >
-              {item.item}
-            </label>
-            <button onClick={() => handleDelete(item.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-    ): (
-      <p style={{marginTop:'2rem'}}>Your list is Empty.</p>
-    )}
+      {items.length ? (
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <p style={{ marginTop: "2rem" }}>Your list is Empty.</p>
+      )}
     </main>
   );
 };
--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Footer = () => {
+const Footer = ({length}) => {
     const today = new Date();
 
   return (
     <footer>
+    <p>{length} {length===1?"item":" items"} in the List </p>
     <p> Copyright &copy; {today.getFullYear()}</p>
     </footer>
   )
